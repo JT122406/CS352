13542dfbd396a8a81559d4d6240989621a4b0849
Joseph T. McQuigg <j.t.mcquigg12@gmail.com>
2023-10-05 13:21:27 -0400

    Take in and store commandline arguments
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

---
+++ Message Validating Client and Server/client.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == "__main__":
+    name = sys.argv[1]
+    port = sys.argv[2]
+    messageFile = sys.argv[3]
+    signatureFile = sys.argv[4]
---
+++ Message Validating Client and Server/server.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == "__main__":
+    port = sys.argv[1]
+    keyFile = sys.argv[2]
