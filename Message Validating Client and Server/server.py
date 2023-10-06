b9a6dcbbee873de33e880ffd66d6c2369e3032ec
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-05 20:04:16 -0400

    Start on server.py
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Message Validating Client and Server/server.py
+++ Message Validating Client and Server/server.py
@@ -1,5 +1,20 @@
 import sys
+import socket
 
 if __name__ == "__main__":
-    port = sys.argv[1]
-    keyFile = sys.argv[2]
+    port = int(sys.argv[1])
+    keyFileName = sys.argv[2]
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind(('localhost', port))
+    server_socket.listen(1)
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+
+        data = client_socket.recv(1024).decode().strip()
+        if data != 'HELLO':
+            print("Error: Invalid message")
+            client_socket.close()
+            break
