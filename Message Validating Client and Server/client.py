9d3a4a858537601135353f47173297354c6b91d7
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-05 20:04:10 -0400

    Start on server.py
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Message Validating Client and Server/client.py
+++ Message Validating Client and Server/client.py
@@ -3,5 +3,7 @@ import sys
 if __name__ == "__main__":
     name = sys.argv[1]
     port = sys.argv[2]
-    messageFile = sys.argv[3]
-    signatureFile = sys.argv[4]
+    messageFileName = sys.argv[3]
+    signatureFileName = sys.argv[4]
+
+    messageFile = open(messageFileName)
