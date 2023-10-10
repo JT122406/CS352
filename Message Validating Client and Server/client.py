9ff06761284c32c64a53dd1010ac5df56486e4d7
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-10 10:37:45 -0400

    read file line by line convert to bytes
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Message Validating Client and Server/client.py
+++ Message Validating Client and Server/client.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
     signatureFileName = sys.argv[4]
 
     messageFile = open(messageFileName)
+    Lines = messageFile.readlines()
+
+    for line in Lines:
+        bytesT = bytes(line, 'utf-8')
