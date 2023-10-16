06b7603decbd179360edf38581bdbb8be0b842ac
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-15 20:42:54 -0400

    Refactor this
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Message Validating Client and Server/server.py
+++ Message Validating Client and Server/server.py
@@ -3,7 +3,7 @@ import sys
 import socket
 
 
-def serverstart(socket1, port, address):
+def serverStart(socket1, port, address):
     socketserver = socket1.socket(socket1.AF_INET, socket1.SOCK_STREAM)
     socketserver.bind((address, port))
     socketserver.listen(1)
@@ -16,7 +16,7 @@ def getKeys():
 
 
 if __name__ == "__main__":
-    server_socket = serverstart(socket, int(sys.argv[1]), 'localhost')
+    server_socket = serverStart(socket, int(sys.argv[1]), 'localhost')
     keys = getKeys()
 
     try:
