71e079df8e3f5915855285ddb5f82fef843eb68a
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-17 17:05:04 -0400

    Pushing this
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Message Validating Client and Server/client.py
+++ Message Validating Client and Server/client.py
@@ -1,51 +1,58 @@
 import sys
 import socket
 
+#python3 client.py localhost 62 message1.txt signature1.txt
+
 if __name__ == "__main__":
     name = sys.argv[1]
     port = sys.argv[2]
     messageFileName = sys.argv[3]
     signatureFileName = sys.argv[4]
 
-    #messageFile = open(messageFileName)
-    #Lines = messageFile.readlines()
+    # messageFile = open(messageFileName)
+    # Lines = messageFile.readlines()
 
-    #for line in Lines:
-        #bytesT = bytes(line, 'utf-8')
+    # for line in Lines:
+    # bytesT = bytes(line, 'utf-8')
     with open(messageFileName, 'r') as file:
-        message = [line.strip().encode() for line in file ]
+        message = [line.strip().encode() for line in file]
     with open(signatureFileName, 'r') as s_file:
-        signature = [line.strip() for line in s_file ]
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((name, int(port)))
+        signature = [line.strip() for line in s_file]
 
-    client_socket.send(b"HELLO\n")
-    response = client_socket.recv(1024).decode().strip()
-    if response != "260 OK":
-        print("Error: Server response not as expected")
-        exit(1)
-    message_counter = 0
-    for message, signature in zip(message, signature):
-        client_socket.send(b"DATA\n")
-        client_socket.send(message)
-        response = client_socket.recv(1024).decode().strip()
-        if response != "270 SIG":
-            print("Error: Server response not as expected")
-            exit(1)
-        server_signature = client_socket.recv(1024).decode().strip()
-        if server_signature == signature:
-            client_socket.send(b"PASS\n")
-        else:
-            client_socket.send(b"FAIL\n")
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((name, int(port)))
+        print("Connection to: ", name, ":", port)
+        client_socket.send("HELLO\n".encode('utf-8'))
+        print("HELLO")
+        exit()
         response = client_socket.recv(1024).decode().strip()
         if response != "260 OK":
             print("Error: Server response not as expected")
             exit(1)
-        message_counter += 1
-    client_socket.send(b"QUIT\n")
-    response = client_socket.recv(1024).decode().strip()
-    if response != "260 OK":
-        print("Error: Server response not as expected")
+        message_counter = 0
+        for message, signature in zip(message, signature):
+            client_socket.send(b"DATA\n")
+            client_socket.send(message)
+            response = client_socket.recv(1024).decode().strip()
+            if response != "270 SIG":
+                print("Error: Server response not as expected")
+                exit(1)
+            server_signature = client_socket.recv(1024).decode().strip()
+            if server_signature == signature:
+                client_socket.send(b"PASS\n")
+            else:
+                client_socket.send(b"FAIL\n")
+            response = client_socket.recv(1024).decode().strip()
+            if response != "260 OK":
+                print("Error: Server response not as expected")
+                exit(1)
+            message_counter += 1
+        client_socket.send(b"QUIT\n")
+        response = client_socket.recv(1024).decode().strip()
+        if response != "260 OK":
+            print("Error: Server response not as expected")
+            exit(1)
+    except Exception as e:
+        print(e)
         exit(1)
-
-    
--- Message Validating Client and Server/server.py
+++ Message Validating Client and Server/server.py
@@ -9,6 +9,7 @@ def serverStart(socket1, port, address):
     socketserver.listen(1)
     return socketserver
 
+#python3 server.py 62 key.txt
 
 def getKeys():
     with open(sys.argv[2], 'r') as file:
@@ -18,38 +19,40 @@ def getKeys():
 if __name__ == "__main__":
     server_socket = serverStart(socket, int(sys.argv[1]), 'localhost')
     keys = getKeys()
-
+    print(keys)
     try:
         client_socket, client_address = server_socket.accept()
-
-        if client_socket.recv(1024).decode('utf-8').strip() != 'HELLO':
+        one = client_socket.recv(1024).decode('utf-8').strip()
+        print(one)
+        print("Connection from: ", client_address)
+        if one != 'HELLO':
             print("Error: Invalid message")
             client_socket.close()
             exit()
 
         while True:
             i = 0
-            match client_socket.recv(1024).decode().strip():
+            match client_socket.recv(1024).decode('utf-8').strip():
                 case 'DATA':
                     sha256_hash = hashlib.sha256()
                     while True:
-                        message = client_socket.recv(1024).decode().strip()
+                        message = client_socket.recv(1024).decode('utf-8').strip()
                         if message == '.':
                             break
-                        sha256_hash.update(message.encode())
-                        sha256_hash.update(keys[i].encode())
+                        sha256_hash.update(message.encode('utf-8'))
+                        sha256_hash.update(keys[i].encode('utf-8'))
                         i += 1
                         sign = sha256_hash.hexdigest()
-                        client_socket.send("270SIG\n".encode())
-                        client_socket.send(sign.encode())
+                        client_socket.send("270SIG\n".encode('utf-8'))
+                        client_socket.send(sign.encode('utf-8'))
 
-                        message = client_socket.recv(1024).decode().strip()
+                        message = client_socket.recv(1024).decode('utf-8').strip()
                         if message != 'PASS' and message != 'FAIL':
                             print("error")
                             client_socket.close()
                             exit()
                         else:
-                            client_socket.send("260OK\n".encode())
+                            client_socket.send("260OK\n".encode('utf-8'))
 
                 case 'QUIT':
                     server_socket.close()
