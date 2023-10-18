b98f74b60e3ef7a1d995e05fdb0b023f964bda60
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-17 23:11:42 -0400

    Server is Perfect I swear
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Message Validating Client and Server/server.py
+++ Message Validating Client and Server/server.py
@@ -28,50 +28,41 @@ def encodeMessage(toEncode):
 if __name__ == "__main__":
     server_socket = serverStart(socket, int(sys.argv[1]), 'localhost')
     keys = getKeys()
-    print(keys)
     try:
         client_socket, client_address = server_socket.accept()
-        one = decodeMessage(client_socket.recv(1024))
-        print(one)
-        print("Connection from: ", client_address)
-        if one != 'HELLO':
-            print("Error: Invalid message 1")
+        if decodeMessage(client_socket.recv(1024)) != 'HELLO':
+            print("Error: Invalid message")
             client_socket.close()
             exit()
 
-        client_socket.send("260 OK\n".encode('ascii'))
+        client_socket.send(encodeMessage("260 OK\n"))  # Everything UP to here works perfectly
+
         while True:
-            i = 0
-            print("Waiting for message")
-            string1 = decodeMessage(client_socket.recv(1024))
-            print(string1)
-            match string1:
+            match decodeMessage(client_socket.recv(1024)):
                 case 'DATA':
-                    while True:
-                        sha256_hash = hashlib.sha256()
+                    for keys1 in keys:
                         message = decodeMessage(client_socket.recv(1024))
-                        if message == '.':
+                        if message == '\\.\\r\\n':
                             break
-                        sha256_hash.update(encodeMessage(message))
-                        sha256_hash.update(encodeMessage(keys[i]))
-                        i += 1
-                        sign = sha256_hash.hexdigest()
-                        print(sign)
                         client_socket.send(encodeMessage("270 SIG\n"))
-                        client_socket.send(encodeMessage(sign + '\n'))
+                        sha256_hash = hashlib.sha256()
+                        sha256_hash.update(encodeMessage(message))
+                        sha256_hash.update(encodeMessage(keys1))
+                        client_socket.send(encodeMessage(sha256_hash.hexdigest() + '\n'))
 
                         message = client_socket.recv(1024).decode('ascii').strip()
-                        if message != 'PASS' and message != 'FAIL':
+                        if message == 'PASS' or message == 'FAIL':
+                            client_socket.send(encodeMessage("260 OK\n"))
+                        else:
                             print("error")
                             client_socket.close()
                             exit()
-                        else:
-                            client_socket.send(encodeMessage("260 OK\n"))
+
                 case 'QUIT':
                     server_socket.close()
-                    exit()
+                    exit(1)
                 case _:
-                    print("Error: Invalid message 2")
+                    print("Error: Invalid message")
                     client_socket.close()
                     exit()
     except Exception as e:
