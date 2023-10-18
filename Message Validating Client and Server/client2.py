a08c09db925790d0d81bfbd566a52823f7a2b51d
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-18 01:53:04 -0400

    Client Rewrite
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

---
+++ Message Validating Client and Server/client2.py
@@ -0,0 +1,82 @@
+import socket
+import sys
+
+def startClient(address, port):
+    socket1 = None
+    connected = False
+    while not connected:
+        try:
+            socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket1.connect((address, port))
+            connected = True
+        except Exception:
+            pass
+
+    return socket1
+
+def getMessages(file):
+    i = 1
+    messages = []
+    with open(file, 'r') as file:
+        for line in file.readlines():
+            if i % 2 == 0:
+                messages.append(line.strip())
+            i += 1
+    return messages
+
+def getSignatures(file):
+    with open(file, 'r') as file:
+        return [line.strip() for line in file.readlines()]
+
+
+def decodeMessage(toDecode):
+    return toDecode.decode('ascii').strip()
+
+
+def encodeMessage(toEncode):
+    return toEncode.encode('ascii')
+
+
+def main(socket):
+    messages = getMessages(sys.argv[3])
+    signatures = getSignatures(sys.argv[4])
+    try:
+        socket.send(encodeMessage("HELLO\n"))
+        if decodeMessage(socket.recv(1024)) != "260 OK":
+            print("Error: Server response not as expected")
+            socket.close()
+            exit(1)
+
+        for message in messages:
+            socket.send(encodeMessage("DATA\n"))
+            socket.send(encodeMessage(message + "\n"))
+            response = decodeMessage(socket.recv(1024))
+            if response != '270 SIG':
+                print("Error: Server response not as expected")
+                socket.close()
+                exit(1)
+            response2 = decodeMessage(socket.recv(1024))
+            if response2 == signatures[messages.index(message)]:
+                socket.send(encodeMessage("PASS\n"))
+            else:
+                socket.send(encodeMessage("FAIL\n"))
+
+            response3 = decodeMessage(socket.recv(1024))
+            if response3 != "260 OK":
+                print("Error: Server response not as expected")
+                socket.close()
+                exit(1)
+
+        socket.send(encodeMessage("QUIT\n"))
+
+    except Exception as e:
+        print(e)
+        socket.close()
+        exit(1)
+
+
+    return 0
+
+
+if __name__ == "__main__":
+    main(startClient(sys.argv[1], sys.argv[2]))
