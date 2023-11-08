2fe6457443a522fea001c6b2f9ded5caa942c394
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-08 12:08:20 -0500

    Setup HTTP Server with User Authentication and File Serving
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

---
+++ HTTP Server with User Authentication and File Serving/sample.sh
@@ -0,0 +1,14 @@
+# Set your server IP and port
+SERVER_IP="127.0.0.1"
+SERVER_PORT="8080"
+
+# Variables for storing session cookies
+SESSION_COOKIE=""
+
+# Common curl options for HTTP/1.0 and connection close
+CURL_OPTIONS="--http1.0 --connect-timeout 5 --max-time 10 --fail --silent"
+
+SESSION_COOKIE=$(curl -i -v -X POST -H "username: Richard" -H "password: 3TQI8TB39DFIMI6" "http://${SERVER_IP}:${SERVER_PORT}/" | grep -i 'Set-Cookie' | cut -d ' ' -f 2 | cut -d '=' -f 2)
+echo "\\nCookie (sessionID) for user: $SESSION_COOKIE"
+
+curl $CURL_OPTIONS -v -X GET -H "Cookie: sessionID=$SESSION_COOKIE" "http://${SERVER_IP}:${SERVER_PORT}/file.txt"
