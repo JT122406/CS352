0e76b0779fd6714b3fe71597554911aa1764375c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-11 13:11:36 -0500

    Read in arguments and start server
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- HTTP Server with User Authentication and File Serving/server.py
+++ HTTP Server with User Authentication and File Serving/server.py
@@ -1,5 +1,19 @@
+import socket
+import sys
+
+
+def serverStart(socket1, port, address):
+    socketserver = socket1.socket(socket1.AF_INET, socket1.SOCK_STREAM)
+    socketserver.bind((address, port))
+    socketserver.listen(1)
+    return socketserver
+
+
 def main():
-    pass
+    server_socket = serverStart(socket, int(sys.argv[2]), sys.argv[1])
+    file = open(sys.argv[3], "r")
+    timeout = sys.argv[4]
+    userDirect = sys.argv[5]
 
 
 if __name__ == "__main__":
