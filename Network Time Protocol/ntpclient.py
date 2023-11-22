ea849bbf5d85afbe93a9824ee748f3cfb7af46ea
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-22 11:19:06 -0500

    From deprecated method usages in ntpclient.py
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Network Time Protocol/ntpclient.py
+++ Network Time Protocol/ntpclient.py
@@ -8,8 +8,8 @@ DO NOT CHANGE ANY OF THE FUNCTION SIGNATURES BELOW
 """
 
 from socket import socket, AF_INET, SOCK_DGRAM
-import struct, socket
-from datetime import datetime
+import struct
+from datetime import datetime, UTC
 
 
 def getNTPTimeValue(server="time.apple.com", port=123) -> (bytes, float, float):
@@ -19,22 +19,22 @@ def getNTPTimeValue(server="time.apple.com", port=123) -> (bytes, float, float):
     address = (host, port)
     msg = b'\x1b' + 47 * b'\0'
 
-    T1 = datetime.utcnow()
-    soc = socket.socket(AF_INET, SOCK_DGRAM)
+    T1 = datetime.now(UTC)
+    soc = socket(AF_INET, SOCK_DGRAM)
     soc.sendto(msg, address)
     msg, address = soc.recvfrom(buf)
 
     # t = struct.unpack('!12I', msg)[10]
     struct.unpack('!12I', msg[:48])
-    T4 = datetime.utcnow()
+    T4 = datetime.now(UTC)
     return msg, T1.timestamp(), T4.timestamp()
 
 
 def ntpPktToRTTandOffset(pkt: bytes, T1: float, T4: float) -> (float, float):
     unpacked = struct.unpack('!12I', pkt[:48])
     number = pow(2, 32)
-    T2 = datetime.utcfromtimestamp((unpacked[9] / number) - 2208988800 + unpacked[8])
-    T3 = datetime.utcfromtimestamp((unpacked[11] / number) - 2208988800 + unpacked[10])
+    T2 = datetime.fromtimestamp((unpacked[9] / number) - 2208988800 + unpacked[8], UTC)
+    T3 = datetime.fromtimestamp((unpacked[11] / number) - 2208988800 + unpacked[10], UTC)
     rtt = (T4 - T1) - (T3.timestamp() - T2.timestamp())
     offset = ((T2.timestamp() - T1) + (T3.timestamp() - T4)) / 2
     return rtt, offset
@@ -48,7 +48,7 @@ def getCurrentTime(server="time.apple.com", port=123, iters=20) -> float:
         offsets.append(offset)
 
     currentTime = (sum(offsets) / len(offsets)) + datetime.now().timestamp()
-    return datetime.utcfromtimestamp(currentTime).timestamp()
+    return datetime.fromtimestamp(currentTime, UTC).timestamp()
 
 
 if __name__ == "__main__":
