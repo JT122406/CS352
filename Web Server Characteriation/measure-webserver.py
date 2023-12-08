472318db0a37f77b5c305883cbe47200502b37f7
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-12-08 12:23:16 -0500

    add pcap files
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Web Server Characteriation/measure-webserver.py
+++ Web Server Characteriation/measure-webserver.py
@@ -2,6 +2,10 @@ from scapy.all import *
 import sys
 
 
+def printAverage(averageLatency: float):
+    print("AVERAGELATENCY: " + averageLatency.__str__())
+
+
 def main():
     processed_file = rdpcap(sys.argv[1])
     sessions = processed_file.sessions()
