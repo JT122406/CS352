d5f4c5c2217a69d30f2a66273623c19d564a3a83
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-12-11 13:02:34 -0500

    current stuff
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Web Server Characteriation/measure-webserver.py
+++ Web Server Characteriation/measure-webserver.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 import sys
 
+from scapy.layers.inet import TCP, IP
 from scapy.plist import _PacketList, _Inner
 
 
@@ -28,11 +29,25 @@ def main():
         sessionsC += 1
         print("SESSION: " + str(sessionsC))
         for packetX in sessions[session]:
-            if packetX.haslayer("TCP") and packetX.haslayer("IP"):
+            if packetX.haslayer(TCP) and packetX.haslayer(IP):
+                ip_src = packetX["IP"].src
+                ip_dst = packetX["IP"].dst
+                tcp_sport = packetX["TCP"].sport
+                tcp_dport = packetX["TCP"].dport
+
+                if ip_dst == host and tcp_dport == port:
+                    # HTTP response
+                    responses[(ip_src, tcp_sport)].append(packet.time)
+                elif ip_src == host and tcp_sport == port:
+                    # HTTP request
+                    requests[(ip_dst, tcp_dport)].append(packet.time)
+
                 counter += 1
                 print("PACKET: " + str(counter))
                 print(packetX.summary())
                 print(packetX.fields)
+                print("Sent Time: ", packetX.sent_time)
+                print("Time: ",packetX.time)
 
 
 if __name__ == "__main__":
