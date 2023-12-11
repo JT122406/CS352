ef3c481a7da18bc8ceded15c6344400bd9859e24
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-12-11 11:57:27 -0500

    Learning about stuff
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Web Server Characteriation/measure-webserver.py
+++ Web Server Characteriation/measure-webserver.py
@@ -1,13 +1,19 @@
 from scapy.all import *
 import sys
 
+from scapy.plist import _PacketList, _Inner
+
 
 def printAverage(averageLatency: float):
     print("AVERAGELATENCY: " + averageLatency.__str__())
 
 
-def readProcessFile(pcap_filename: str):
-    processed_file = rdpcap(pcap_filename)
+def readProcessFile(pcap_filename: str) -> Dict[str, _PacketList[_Inner]]:
+    try:
+        processed_file = rdpcap(pcap_filename)
+    except Exception as e:
+        print(f"ERROR: Could not read file {e}")
+        sys.exit(1)
     return processed_file.sessions()
 
 
@@ -16,10 +22,17 @@ def main():
     sessions = readProcessFile(sys.argv[1])
     host = sys.argv[2]
     port = int(sys.argv[3])
+    sessionsC = 0
     counter = 0
     for session in sessions:
+        sessionsC += 1
+        print("SESSION: " + str(sessionsC))
         for packetX in sessions[session]:
-            counter += 1
+            if packetX.haslayer("TCP") and packetX.haslayer("IP"):
+                counter += 1
+                print("PACKET: " + str(counter))
+                print(packetX.summary())
+                print(packetX.fields)
 
 
 if __name__ == "__main__":
