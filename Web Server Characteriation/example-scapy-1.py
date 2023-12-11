e3648115700e469e3ab521d1294ae26fb5a2c437
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-12-11 10:32:59 -0500

    read and process file
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Web Server Characteriation/example-scapy-1.py
+++ Web Server Characteriation/example-scapy-1.py
@@ -20,28 +20,29 @@ load_layer("http")
 pcap_filename = "pcap1.pcap"
 
 # example counters 
-number_of_packets_total = 0  
+number_of_packets_total = 0
 number_of_tcp_packets = 0
 number_of_udp_packets = 0
 
 processed_file = rdpcap(pcap_filename)  # read in the pcap file 
-sessions = processed_file.sessions()    #  get the list of sessions 
-for session in sessions:                   
-    for packet in sessions[session]:    # for each packet in each session
-        number_of_packets_total = number_of_packets_total + 1  #increment total packet count 
-        if packet.haslayer(TCP):        # check is the packet is a TCP packet
-            number_of_tcp_packets = number_of_tcp_packets + 1   # count TCP packets 
-            source_ip = packet[IP].src   # note that a packet is represented as a python hash table with keys corresponding to 
-            dest_ip = packet[IP].dst     # layer field names and the values of the hash table as the packet field values
-            
+sessions = processed_file.sessions()  # get the list of sessions
+for session in sessions:
+    for packet in sessions[session]:  # for each packet in each session
+        number_of_packets_total = number_of_packets_total + 1  # increment total packet count
+        if packet.haslayer(TCP):  # check is the packet is a TCP packet
+            number_of_tcp_packets = number_of_tcp_packets + 1  # count TCP packets
+            source_ip = packet[IP].src # note that a packet is represented as a python hash table with keys corresponding to
+            dest_ip = packet[IP].dst # layer field names and the values of the hash table as the packet field values
+
             if packet.haslayer(HTTP):
-                if HTTPRequest in packet:   
+                if HTTPRequest in packet:
                     arrival_time = packet.time
-                    print ("Got a TCP packet part of an HTTP request at time: %0.4f for server IP %s" % (arrival_time,dest_ip))
+                    print("Got a TCP packet part of an HTTP request at time: %0.4f for server IP %s" % (
+                        arrival_time, dest_ip))
                     packet.show()
         else:
             if packet.haslayer(UDP):
                 number_of_udp_packets = number_of_udp_packets + 1
-                
-print("Got %d packets total, %d TCP packets and %d UDP packets" % (number_of_packets_total, number_of_tcp_packets,number_of_udp_packets))
 
+print("Got %d packets total, %d TCP packets and %d UDP packets" % (
+    number_of_packets_total, number_of_tcp_packets, number_of_udp_packets))
--- Web Server Characteriation/measure-webserver.py
+++ Web Server Characteriation/measure-webserver.py
@@ -6,17 +6,20 @@ def printAverage(averageLatency: float):
     print("AVERAGELATENCY: " + averageLatency.__str__())
 
 
+def readProcessFile(pcap_filename: str):
+    processed_file = rdpcap(pcap_filename)
+    return processed_file.sessions()
+
+
 def main():
     load_layer("http")
-    processed_file = rdpcap(sys.argv[1])
-    sessions = processed_file.sessions()
+    sessions = readProcessFile(sys.argv[1])
+    host = sys.argv[2]
+    port = int(sys.argv[3])
     counter = 0
     for session in sessions:
         for packetX in sessions[session]:
             counter += 1
-
-    host = sys.argv[2]
-    port = int(sys.argv[3])
 
 
 if __name__ == "__main__":
